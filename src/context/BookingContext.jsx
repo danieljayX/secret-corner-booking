d1cbe46c550ab990958e90564d5b5df29acf7ae7
daniel legumbres <legumbresdaniel1@gmail.com>
2026-05-17 20:47:52 +0800

    refactor: remove GCash and Maya reservation fee workflow per user request to prevent customer scam hesitation

--- src/context/BookingContext.jsx
+++ src/context/BookingContext.jsx
@@ -179,9 +179,7 @@ export function BookingProvider({ children }) {
         time: '14:00',
         status: 'Confirmed',
         customerPhone: '09123456789',
-        socialLink: 'https://facebook.com/juandelacruz',
-        paymentMethod: 'GCash',
-        referenceNumber: '7041234567890'
+        socialLink: 'https://facebook.com/juandelacruz'
       },
       {
         id: 2,
@@ -194,9 +192,7 @@ export function BookingProvider({ children }) {
         time: '15:00',
         status: 'Pending',
         customerPhone: '09223334444',
-        socialLink: 'https://instagram.com/mariasantos',
-        paymentMethod: 'Maya',
-        referenceNumber: '5029876543210'
+        socialLink: 'https://instagram.com/mariasantos'
       },
       {
         id: 3,
@@ -209,9 +205,7 @@ export function BookingProvider({ children }) {
         time: '11:00',
         status: 'Declined',
         customerPhone: '09445556666',
-        socialLink: 'https://facebook.com/robertgarcia',
-        paymentMethod: 'GCash',
-        referenceNumber: '7095555666677'
+        socialLink: 'https://facebook.com/robertgarcia'
       }
     ];
   });
